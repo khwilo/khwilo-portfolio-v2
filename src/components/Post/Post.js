dc1709706d6e9d770d057ea4804e90b22a864ca8
Khwilo Kabaka <khwilowatai@gmail.com>
2020-04-24 18:13:47 +0300

    Refactor a Post into its own component and render the list

---
+++ src/components/Post/Post.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Post = ({ title, publishedDate, link }) => {
+  return (
+    <a
+      href={link}
+      target='_blank'
+      rel='noreferrer noopener'
+      className='post__container-link'
+      aria-label='published online content'
+    >
+      <article className='post'>
+        <h3 className='post_title'>{title}</h3>
+        <p className='post__date'>{publishedDate}</p>
+      </article>
+    </a>
+  );
+};
+
+export default Post;
--- src/containers/Posts/Posts.js
+++ src/containers/Posts/Posts.js
@@ -1,53 +1,44 @@
 import React from 'react';
 
+import Post from '../../components/Post/Post';
+
 const Posts = () => {
+  const postsList = [
+    {
+      title: "Display a user's password on input",
+      publishedDate: 'Feb 13, 2020',
+      link: 'https://dev.to/khwilo/til-form-password-reveal-g37',
+    },
+    {
+      title: 'Get started with making your Angular web app offline-capable',
+      publishedDate: 'Feb 7, 2020',
+      link:
+        'https://dev.to/khwilo/get-started-with-making-your-angular-web-app-offline-capable-1e9n',
+    },
+    {
+      title:
+        'How to create full-screen drawer navigation in HTML, CSS, and JavaScript',
+      publishedDate: 'Jan 24, 2020',
+      link:
+        'https://dev.to/khwilo/how-to-create-full-screen-drawer-navigation-in-html-css-and-javascript-44oe',
+    },
+  ];
+
+  const postsView = postsList.map(({ title, publishedDate, link }, index) => {
+    return (
+      <Post
+        key={index}
+        title={title}
+        publishedDate={publishedDate}
+        link={link}
+      />
+    );
+  });
   return (
     <section id='writing' className='section__main posts'>
       <h2 className='heading posts_heading'>Writing</h2>
 
-      <div className='posts__wrapper'>
-        <a
-          href='https://dev.to/khwilo/til-form-password-reveal-g37'
-          target='_blank'
-          rel='noreferrer noopener'
-          className='post__container-link'
-          aria-label='published online content'
-        >
-          <article className='post'>
-            <h3 className='post_title'>Display a user's password on input</h3>
-            <p className='post__date'>Feb 13, 2020</p>
-          </article>
-        </a>
-        <a
-          href='https://dev.to/khwilo/get-started-with-making-your-angular-web-app-offline-capable-1e9n'
-          target='_blank'
-          rel='noreferrer noopener'
-          className='post__container-link'
-          aria-label='published online content'
-        >
-          <article className='post'>
-            <h3 className='post_title'>
-              Get started with making your Angular web app offline-capable
-            </h3>
-            <p className='post__date'>Feb 7, 2020</p>
-          </article>
-        </a>
-        <a
-          href='https://dev.to/khwilo/how-to-create-full-screen-drawer-navigation-in-html-css-and-javascript-44oe'
-          target='_blank'
-          rel='noreferrer noopener'
-          className='post__container-link'
-          aria-label='published online content'
-        >
-          <article className='post'>
-            <h3 className='post_title'>
-              How to create full-screen drawer navigation in HTML, CSS, and
-              JavaScript
-            </h3>
-            <p className='post__date'>Jan 24, 2020</p>
-          </article>
-        </a>
-      </div>
+      <div className='posts__wrapper'>{postsView}</div>
     </section>
   );
 };
