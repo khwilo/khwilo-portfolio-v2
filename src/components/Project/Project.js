1e7bf6fd539e0cb4a98825e16b228bd8aaaa584a
Khwilo Kabaka <khwilowatai@gmail.com>
2020-04-24 17:54:56 +0300

    Refactor a project into its component and render the list

---
+++ src/components/Project/Project.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Project = ({ title, descriptionTag, image, link }) => {
+  return (
+    <article className='project'>
+      <div className='project__image-wrapper'>
+        <a href={link} className='project__item' aria-label='project item'>
+          <img src={`/images/${image}`} alt='' className='project__image' />
+        </a>
+      </div>
+      <div className='project__description'>
+        <h3 className='project__description-title'>{title}</h3>
+        <p className='project__description-tag'>{descriptionTag}</p>
+      </div>
+    </article>
+  );
+};
+
+export default Project;
--- src/containers/Projects/Projects.js
+++ src/containers/Projects/Projects.js
@@ -1,77 +1,48 @@
 import React from 'react';
 
+import Project from '../../components/Project/Project';
+
 const Projects = () => {
+  const projectList = [
+    {
+      title: 'Countries Info',
+      descriptionTag: 'Responsive Design, Grid Layout, Themes, PWA',
+      image: 'countries-info.png',
+      link: 'https://countries-info.netlify.app/countries',
+    },
+    {
+      title: 'Questioner API',
+      descriptionTag: 'REST API, Flask, PostgreSQL',
+      image: 'questioner-api.png',
+      link: 'https://q-questioner-api.herokuapp.com/apidocs/',
+    },
+    {
+      title: 'Chill Room',
+      descriptionTag: 'Responsive Design, Grid Layout',
+      image: 'chill-room.png',
+      link: 'https://chill-room.netlify.com/',
+    },
+  ];
+
+  const projectsView = projectList.map(
+    ({ title, descriptionTag, image, link }, index) => {
+      return (
+        <Project
+          key={index}
+          title={title}
+          descriptionTag={descriptionTag}
+          image={image}
+          link={link}
+        />
+      );
+    }
+  );
+
   return (
     <section id='projects' className='section__main projects'>
       <h2 className='heading projects__heading'>Projects</h2>
 
-      <div className='projects__wrapper'>
-        <article className='project'>
-          <div className='project__image-wrapper'>
-            <a
-              href='https://countries-info.netlify.app/countries'
-              className='project__item'
-              aria-label='project item'
-            >
-              <img
-                src='/images/countries-info.png'
-                alt=''
-                className='project__image'
-              />
-            </a>
-          </div>
-          <div className='project__description'>
-            <h3 className='project__description-title'>Countries Info</h3>
-            <p className='project__description-tag'>
-              Responsive Design, Grid Layout, Themes, PWA
-            </p>
-          </div>
-        </article>
-
-        <article className='project'>
-          <div className='project__image-wrapper'>
-            <a
-              href='https://q-questioner-api.herokuapp.com/apidocs/'
-              className='project__item'
-              aria-label='project item'
-            >
-              <img
-                src='/images/questioner-api.png'
-                alt=''
-                className='project__image'
-              />
-            </a>
-          </div>
-          <div className='project__description'>
-            <h3 className='project__description-title'>Questioner API</h3>
-            <p className='project__description-tag'>
-              REST API, Flask, PostgreSQL
-            </p>
-          </div>
-        </article>
-
-        <article className='project'>
-          <div className='project__image-wrapper'>
-            <a
-              href='https://chill-room.netlify.com/'
-              className='project__item'
-              aria-label='project item'
-            >
-              <img
-                src='/images/chill-room.png'
-                alt=''
-                className='project__image'
-              />
-            </a>
-          </div>
-          <div className='project__description'>
-            <h3 className='project__description-title'>Chill Room</h3>
-            <p className='project__description-tag'>
-              Responsive Design, Grid Layout
-            </p>
-          </div>
-        </article>
-      </div>
+      <div className='projects__wrapper'>{projectsView}</div>
     </section>
   );
 };
