aca85258cbfba366aa9392a41a663726d4be12f4
Khwilo Kabaka <khwilowatai@gmail.com>
2020-04-27 21:04:38 +0300

    Re-arrange the order of elements

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -5,19 +5,21 @@ import './Banner.css';
 const Banner = () => {
   return (
     <section className='banner'>
-      <div className='intro'>
-        <h1 className='intro__name'>Hi, I'm Khwilo Kabaka</h1>
-        <p className='intro__profession'>Mobile Web Application Developer</p>
-        <a href='mailto:khwilowatai@gmail.com' className='email-address'>
-          khwilowatai@gmail.com
-        </a>
-      </div>
       <div className='profile'>
         <img
           className='profile-image'
           src='/images/khwilo-portrait.jpeg'
           alt='Khwilo Kabaka portrait'
         />
+      </div>
+      <div className='intro'>
+        <h1 className='intro__name'>Hi, I'm Khwilo Kabaka</h1>
+        <div class='intro__details'>
+          <p className='intro__profession'>Mobile Web Application Developer</p>
+          <a href='mailto:khwilowatai@gmail.com' className='email-address'>
+            khwilowatai@gmail.com
+          </a>
+        </div>
       </div>
     </section>
   );
